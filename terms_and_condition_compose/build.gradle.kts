cb667d4c87871f5efa2bcbe3b6185abfe71cd9e4
Tushar Gogna <tushar47@gmail.com>
2023-09-28 19:03:10 -0400

    Made changes to gradle in order to better publish the lib

--- terms_and_condition_compose/build.gradle.kts
+++ terms_and_condition_compose/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -39,13 +40,20 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.tushargogna"
+            artifactId = "TermsText_Compose"
+            version = "1.0"
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
